567345a97f60cf9fd2d9c48212af220bb49dc7ab
Kira EB <107140669+KemmettBuck@users.noreply.github.com>
2026-07-13 09:57:02 -0400

    import javaFX features, create class and primary stage and GridPane

---
+++ Exercise14_01.java
@@ -0,0 +1,22 @@
+/**
+ * Exercise14_01
+ * Display flag images in 2x2 Grid using JavaFX
+ */
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Exercise14_01 {
+    @Override
+    public void start(Stage primaryStage) {
+        //Create Grid
+        GridPane gridPane = new GridPane();
+
+        // Load flag images
+    }
+}
