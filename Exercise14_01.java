25ead11310344b5df5c92f819ee376a4f774a1aa
Kira EB <107140669+KemmettBuck@users.noreply.github.com>
2026-07-13 10:34:44 -0400

    change image loading to use file URI to find images

--- Exercise14_01.java
+++ Exercise14_01.java
@@ -18,10 +18,10 @@ public class Exercise14_01 extends Application {
         GridPane gridPane = new GridPane();
 
         // Load flag images
-        Image flag1 = new Image("flag1.gif"); // US
-        Image flag2 = new Image("flag2.gif"); // UK
-        Image flag6 = new Image("flag6.gif"); // France
-        Image flag7 = new Image("flag7.gif"); // China
+        Image flag1 = new Image(new java.io.File("flag1.gif").toURI().toString()); // US
+        Image flag2 = new Image(new java.io.File("flag2.gif").toURI().toString()); // UK
+        Image flag6 = new Image(new java.io.File("flag6.gif").toURI().toString()); // France
+        Image flag7 = new Image(new java.io.File("flag7.gif").toURI().toString()); // China
 
         // Create ImageView for each flag
         ImageView imageView1 = new ImageView(flag1);
